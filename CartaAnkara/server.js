3021978484488c0aec3b32afc221f0ac9db22555
Tomás <tomivignau@gmail.com>
2025-10-23 00:07:20 -0300

    CambiosConfig2

--- CartaAnkara/server.js
+++ CartaAnkara/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "dist")));
 
 // Ruta pública específica que querés
-app.get("/qr", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
